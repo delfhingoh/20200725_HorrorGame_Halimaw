6c5a2fb64915426690dbed4b8806357a95ee8eea
Delfhin GOH <delfhingoh98@gmail.com>
2023-01-31 23:42:43 +0800

    Revert "Update"
    This reverts commit 9cf1694a0cf9bd50f00bdcc62ac3a1d1e2dea9a6.

--- Codes/ICanBeInteracted.cs
+++
@@ -1,9 +0,0 @@
-﻿/*
- * ICanBeInteracted INTERFACE: This is an interface to be used on objects
- * that are interactables via MOUSE or KEYBOARD.
- */
-public interface ICanBeInteracted<T>
-{
-    void PickableInteraction(T _whichHand);
-    void MouseClickInteraction();
-}
